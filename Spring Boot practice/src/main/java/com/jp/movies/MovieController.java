80c44090b004132dff8b29467e8f0abc3718d63f
pablozap <124183708+pablozap@users.noreply.github.com>
2024-11-25 21:24:45 -0500

    Add files via upload

---
+++ Spring Boot practice/src/main/java/com/jp/movies/MovieController.java
@@ -0,0 +1,28 @@
+package com.jp.movies;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
+    }
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<>(movieService.singleMovie(imdbId), HttpStatus.OK);
+    }
+}
